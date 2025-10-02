9b87ccf326de020ea87ab08fcf98ae5b2b6e7a1a
Noor-Jaha <noorj8306033@gmail.com>
2025-10-02 19:06:34 +0530

    7th commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+
 
 const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+
+
   return (
-    <div>
-      <h1>contact</h1>
-    </div>
+    <section className='section-contact'>
+      <h2 className='container-title'>Contact Us</h2>
+        
+     <div className='contact-wrapper container'>
+       <form action={handleFormSubmit}>
+        <input 
+        type="text"
+        className='form-control'
+        name='username'
+        placeholder='Enter Your Name'
+        required
+        autoComplete='false'
+         />
+
+         <input 
+        type="Email"
+        className='form-control'
+        name='Email'
+        placeholder='Enter Your Email'
+        required
+        autoComplete='false'
+         />
+
+         <textarea
+          className='form-control'
+          rows='10'
+          name="message"
+          placeholder='Enter Your Message'
+          required
+          autoComplete='false'
+          ></textarea>
+
+          <button type='submit' value='send' >Send</button>
+        </form> 
+      </div> 
+    </section>
   )
 }
 
