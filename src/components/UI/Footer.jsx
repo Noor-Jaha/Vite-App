7278dcf8b47e1a7a5f5b92d4adf9cd861735412d
Noor-Jaha <noorj8306033@gmail.com>
2025-10-03 14:29:21 +0530

    8th commit

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,10 +1,60 @@
-
+import { MdPlace } from 'react-icons/md';
+import footerContact from '../../api/footerApi.json'
+import { IoCallSharp } from 'react-icons/io5';
+import { TbMailPlus } from 'react-icons/tb';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
+  const footerIcon = {
+    MdPlace: <MdPlace/>,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus/>
+  }
   return (
-    <div>
-      <h1>footer</h1>
+   <footer className="footer-section">
+    <div className="container grid grid-three-cols">
+      {
+       footerContact.map((elm, index) => {
+         const {icon, title, details} = elm;
+         return (
+          <div className='footer-contact' key={index}>
+            <div className='icon'>{footerIcon[icon]}</div>
+            <div className='footer-contact-text'>
+              <p>{title}</p>
+              <p>{details}</p>
+            </div>
+          </div>
+         )
+       })
+      }
     </div>
+      <div className='copyright-area'>
+   <div className='container'>
+    <div className='grid grid-two-cols'>
+      <div className='copyright-text'>
+        <p>
+          Copyright & copy; 2025,All Right Reserved 
+          <NavLink to='https://www.linkedin.com/checkpoint/challenge/AgERZm4M-riQFgAAAZguEFb-PJx_A1XahFMsEpkiissGvcKu8r-tztuhF_tdo8W9i6MavNSuKgwU2TNQCRVW8QQCrxHj9w?ut=3biMPOWOhxRXQ1'>Linked In</NavLink>
+          
+        </p>
+      </div>
+      <div className='footer-menu'>
+        <ul>
+          <li><NavLink to='/'>Home</NavLink>
+          </li>
+
+          <li><NavLink to='https://www.instagram.com/?next=%2F'>Social</NavLink>
+          </li>
+          <li><NavLink to='/contact'>Contact</NavLink>
+          </li>
+              <li><NavLink to='/'>Source code</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+   </div>
+   </div>
+   </footer>
   )
 }
 
